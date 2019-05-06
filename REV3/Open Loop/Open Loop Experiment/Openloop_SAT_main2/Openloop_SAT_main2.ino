f6afff391b067b18484a55d2e5bb01d282f46b95
Ajin Sunny <ajin.sunny@github.com>
2019-05-06 16:41:02 -0400

    Update Openloop_SAT_main2.ino
    Added data logging for open loop experiments.

--- REV3/Open Loop/Open Loop Experiment/Openloop_SAT_main2/Openloop_SAT_main2.ino
+++ REV3/Open Loop/Open Loop Experiment/Openloop_SAT_main2/Openloop_SAT_main2.ino
@@ -1,14 +1,30 @@
 #include <DueTimer.h>
 #include <SineWaveDue.h>
+#include <SD.h>
+#include <SPI.h>
+#include <VL53L0X.h>
+#include "Arduino.h"
+#include "DFRobot_VL53L0X.h"
+
 
 char incomingByte;
+DFRobotVL53L0X sensor;
+unsigned long period = 700000000; 
+float dist[2];
+unsigned int i = 0;
+float vel = 0;
+float velocity; 
+float delta_pos;
+File myFile;
 
-void setup()  { 
+void setup()  
+{ 
   analogReadResolution(10);
   analogWriteResolution(10);
   pinMode(9, OUTPUT);
   Serial.begin(115200);
   //delay(20000);
+  myFile = SD.open("sat2.csv", FILE_WRITE);
   while(Serial.available()==0){}
   incomingByte = Serial.read();
   if(incomingByte == 'A')
@@ -17,13 +33,42 @@ void setup()  {
   }
 } 
 
-void loop()  { 
-  
+void loop()  
+{ 
+  while(millis() < period) 
+  {
+    //if(myFile)
+    {
     S.startSinusoid1(10);
+    Serial.print("Voltage value: ");
+    Serial.println(S.return_voltage());
+    dist[i] = (sensor.getDistance()/1000)+0.2;
+    vel = velocity_func(dist);
+    Serial.print("Distance: ");
+    Serial.println(dist[i]);
+    myFile.print(dist[i]);
+    myFile.print(",");
+    Serial.print("Velocity: ");
+    Serial.println(vel);
+    myFile.print(vel);
+    myFile.print(",");
+    i++;
+
+    
+    if (i >= 2)
+    {
+      i = 0;
+    }
+
+
+    
     delay(1000);
     S.stopSinusoid();
-  
-    
+    }
+  }
+    myFile.close();
+    exit(0);
+}    
 //    sw.playTone2(1000, 1200);
 //    delay(1000);
 //    sw.stopTone();
@@ -36,7 +81,16 @@ void loop()  {
 //      delay(100);
 //  }
     
-} 
+
+
+
+
+float velocity_func(float dist[2])
+{
+  delta_pos = dist[i] - dist[i - 1];
+  velocity = delta_pos/0.016;
+  return velocity;
+}
 
 
   
