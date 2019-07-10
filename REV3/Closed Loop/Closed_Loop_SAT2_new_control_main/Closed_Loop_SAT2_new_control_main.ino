d30eba30cc36381cdb487f9efb8659b41df0709b
Ajin Sunny <ajin.sunny@github.com>
2019-07-10 12:30:18 -0400

    Update Closed_Loop_SAT2_new_control_main.ino
    Updated the filter coefficient.

--- REV3/Closed Loop/Closed_Loop_SAT2_new_control_main/Closed_Loop_SAT2_new_control_main.ino
+++ REV3/Closed Loop/Closed_Loop_SAT2_new_control_main/Closed_Loop_SAT2_new_control_main.ino
@@ -63,7 +63,7 @@ char incomingByte;
 double relative_dist = 0.00;
 double total_relative_dist = 0.00;
 double velocity_final_final = 0.00;
-double a = 0.7;
+double a = 0.3;
 double previous_velocity = 0.00;
 double current_velocity = 0.00;
 
